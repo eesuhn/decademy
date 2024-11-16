a4bf45c63a1420d83f7908f6bca02595e1cb4d78
lewislee42 <minecraftpe132006@gmail.com>
2024-11-16 16:25:32 +0700

    Commented out

--- scripts/contracts.ts
+++ scripts/contracts.ts
@@ -1,5 +1,16 @@
+import dotenv from 'dotenv';
 import { DeploymentConfig } from './types';
 
+dotenv.config();
+
 export const deploymentContracts: DeploymentConfig[] = [
-  
+  {
+    contractName: 'GraphTest',
+    verify: true
+  },
+  // {
+  //   contractName: 'TopicAttestorRelayer',
+  //   verify: true,
+  //   args: [process.env.TOPIC_ATTESTOR_RELAYER_ADDRESS],
+  // },
 ];
