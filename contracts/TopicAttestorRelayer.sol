e9bd19a715f7d31d2721d49eb8c82eae162dd854
lewislee42 <minecraftpe132006@gmail.com>
2024-11-16 15:42:33 +0700

    Added some more functions

--- contracts/TopicAttestorRelayer.sol
+++ contracts/TopicAttestorRelayer.sol
@@ -14,4 +14,65 @@ contract TopicAttestorRelayer is Ownable {
     constructor(address _graphTest) Ownable(_msgSender()) {
         graphTest = GraphTest(_graphTest);
     }
+
+    modifier onlyRelayer() {
+        require(msg.sender == owner(), "Only relayer can call this function");
+        _;
+    }
+
+    function getNonce(address user) external view returns (uint256) {
+        return nonces[user];
+    }
+
+    function metaTxSubmitTopic(
+        address user,
+        string memory topic,
+        string memory description,
+        string memory topicIMG,
+        bytes memory signature
+    ) external onlyRelayer {
+        require(address(msg.sender).balance >= 0.001 ether, "Relayer: Low balance");
+        bytes32 messageHash = keccak256(abi.encodePacked(user, topic, description, topicIMG, nonces[user]));
+        bytes32 ethSignedMessageHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
+        require(SignatureChecker.isValidSignatureNow(user, ethSignedMessageHash, signature), "Invalid signature");
+        nonces[user]++;
+        try graphTest.submitTopic(topic, description, user, topicIMG) {
+        } catch {
+            revert("Transaction failed");
+        }
+    }
+
+    function metaTxAddEducator(
+        address user,
+        address educator,
+        bytes memory signature
+    ) external onlyRelayer {
+        require(address(msg.sender).balance >= 0.001 ether, "Relayer: Low balance");
+        bytes32 messageHash = keccak256(abi.encodePacked(user, educator, nonces[user]));
+        bytes32 ethSignedMessageHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
+        require(SignatureChecker.isValidSignatureNow(user, ethSignedMessageHash, signature), "Invalid signature");
+        nonces[user]++;
+        try graphTest.emitAddEducator(educator) {
+        } catch {
+            revert("Transaction failed");
+        }
+    }
+
+    function metaTxEmitAddUserToModule(
+        address user,
+        address walletAddr,
+        uint256 moduleId,
+        bytes memory signature
+    ) external onlyRelayer {
+        require(address(msg.sender).balance >= 0.001 ether, "Relayer: Low balance");
+        bytes memory encodedParams = abi.encode(walletAddr, moduleId);
+        bytes32 messageHash = keccak256(abi.encodePacked(user, encodedParams, nonces[user]));
+        bytes32 ethSignedMessageHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
+        require(SignatureChecker.isValidSignatureNow(user, ethSignedMessageHash, signature), "Invalid signature");
+        nonces[user]++;
+        try graphTest.emitAddUserToModule(walletAddr, moduleId) {
+        } catch {
+            revert("Transaction failed");
+        }
+    }
 }
