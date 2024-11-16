d19ec5edcad84e1a2bd9581da482126a188daedc
eesuhn <eason.yihong@gmail.com>
2024-11-16 15:38:02 +0700

    I guess it is completed lol

--- contracts/GraphTest.sol
+++ contracts/GraphTest.sol
@@ -80,4 +80,160 @@ contract GraphTest is Ownable {
         spInstance = ISP(instance);
         schemaId = schema;
     }
+
+    // Admin functions
+    function setTrustedRelayer(address _relayer) external onlyOwner {
+        require(_relayer != address(0), "Invalid relayer address");
+        trustedRelayer = _relayer;
+    }
+
+    // Getter
+    // emitTopicAttestationStatus
+    function getTopic(uint256 topicId) external view returns (string memory, string memory, address, string memory, bool) {
+        require(topicId > 0 && topicId <= topicIdCounter, "Invalid topic ID");
+        Topic memory t = topics[topicId];
+        return (t.topic, t.description, t.learner, t.image, t.isAttested);
+    }
+
+    function getTopicCount() external view returns (uint256) {
+        return topicIdCounter;
+    }
+
+    function getPayout(
+        uint256 topicId,
+        uint256 engagement,
+        uint256 totalEngagement,
+        uint256 rating,
+        uint256 totalRating,
+        uint256 learner,
+        uint256 totalLearner
+    ) external view returns (uint256 payoutAmount) {
+        require(totalEngagement > 0, "Total engagement cannot be zero");
+        require(totalRating > 0, "Total rating cannot be zero");
+        require(totalLearner > 0, "Total learners cannot be zero");
+
+        uint256 numerator = engagement * rating * learner * topicStakedPool[topicId]; 
+        uint256 denominator = totalEngagement * totalRating * totalLearner;
+        payoutAmount = numerator / denominator;
+        return payoutAmount;
+    }
+
+    function getTopicStakedPool(uint256 _topicId) external view returns (uint256) {
+        require(_topicId > 0 && _topicId <= topicIdCounter, "Invalid topic ID");
+        return topicStakedPool[_topicId];
+    }
+
+    // Main Functions
+    function emitCreateUser(address walletAddr, string calldata name, string[] calldata socials) public {
+        whoIsVerified[walletAddr] = false;
+        emit createUser(walletAddr, name, socials, whoIsVerified[walletAddr]);
+    }
+
+    function emitAddUserToModule(address walletAddr, uint256 moduleId) public {
+        require(moduleId > 0 && moduleId <= moduleIdCounter, "Error: Invalid moduleId");
+        emit learnerJoinModule(walletAddr, moduleId);
+    }
+
+    function emitAddUserToTopic(address walletAddr, uint256 topicId) public {
+        require(topicId > 0 && topicId <= topicIdCounter, "Error: Invalid topicId");
+        emit learnerJoinTopic(walletAddr, topicId);
+    }
+
+    function emitVerifyWorldID(address walletAddr) public {
+        require(whoIsVerified[walletAddr] == false, "Error: User already verified");
+        whoIsVerified[walletAddr] = true;
+        emit updateWorldIdStatus(walletAddr);
+    }
+
+    function emitCreateModule(
+        uint256 topicId, address eduWalletAddr, uint256 totalPages,
+        string calldata title, string calldata description, string calldata moduleIMG
+    ) public {
+        require(topicId > 0 && topicId <= topicIdCounter, "Error: Invalid topicId");
+        moduleIdCounter++;
+        emit createModule(eduWalletAddr, topicId, moduleIdCounter, totalPages, title, description, moduleIMG);
+    }
+
+    function emitUpdateLearnerProgress(address walletAddr, uint256 moduleId, uint256 pageAmount, uint256 rating) public {
+        require(moduleId > 0 && moduleId <= moduleIdCounter, "Error: Invalid moduleId");
+        emit updateLearnerProgress(walletAddr, moduleId, pageAmount, rating);
+    }
+
+    function submitTopic(string memory topic, string memory description, address learner, string memory topicIMG) external {
+        require(!existingTopics[topic], "Topic already submitted");
+        require(learner != address(0), "Invalid learner address");
+        topicIdCounter++;
+        topics[topicIdCounter] = Topic({
+            topic: topic,
+            description: description,
+            image: topicIMG,
+            learner: learner,
+            isAttested: false
+        });
+        existingTopics[topic] = true;
+        emit createTopic(topicIdCounter, topic, description, topicIMG, false);
+    }
+
+    function emitAttestTopic(uint256 topicId) external onlyEducator returns (uint64) {
+        require(topicId > 0 && topicId <= topicIdCounter, "Invalid topic ID");
+        require(!topics[topicId].isAttested, "Topic already attested");
+        require(msg.sender != topics[topicId].learner, "Educator cannot be the learner");
+        require(msg.sender != owner(), "Owner cannot attest to the topic");
+
+        bytes[] memory recipients = new bytes[](1);
+        recipients[0] = abi.encode(topics[topicId].learner);
+        bytes memory data = abi.encode(
+            topics[topicId].topic,
+            topics[topicId].description,
+            topics[topicId].image
+        );
+        Attestation memory a = Attestation({
+            schemaId: schemaId,
+            linkedAttestationId: 0,
+            attestTimestamp: 0,
+            revokeTimestamp: 0,
+            attester: address(this),
+            validUntil: 0,
+            dataLocation: DataLocation.ONCHAIN,
+            revoked: false,
+            recipients: recipients,
+            data: data
+        });
+        topics[topicId].isAttested = true;
+        uint64 attestationId = spInstance.attest(a, "", "", "");
+        emit updateTopicAttestationStatus(topicId, true);
+        return attestationId;
+    }
+
+    function emitAddEducator(address educator) external {
+        require(msg.sender != owner(), "Contract owner cannot be educator");
+        require(educator != owner(), "Contract owner cannot be educator");
+        require(!educators[educator], "Address is already an educator");
+        educatorPayout[educator] = 0;
+        educators[educator] = true;
+        emit createEducator(educator);
+    }
+
+    function stake(uint256 _topicId) external payable validStakeAmount {
+        require(_topicId <= topicIdCounter && _topicId > 0, "Invalid topic ID");
+        require(!isStaked[msg.sender][_topicId], "Already staked for this topic");
+        isStaked[msg.sender][_topicId] = true;
+        userStakeAmount[msg.sender][_topicId] += msg.value;
+        topicStakedPool[_topicId] += msg.value;
+        emit Staked(
+            msg.sender,
+            _topicId,
+            msg.value,
+            block.timestamp
+        );
+        emit StakePoolUpdated(_topicId,topicStakedPool[_topicId],block.timestamp);
+        emit learnerJoinTopic(msg.sender, _topicId);
+    }
+
+    function withdraw(uint256 _topicId, uint256 amount, address payable recipient) external onlyEducator {
+        require(recipient != address(0), "Invalid recipient address");
+        require(amount > 0 && amount <= topicStakedPool[_topicId], "Insufficient balance for this topic");
+        topicStakedPool[_topicId] -= amount;
+        recipient.transfer(amount);
+    }
 }
